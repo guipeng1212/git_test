ef9a394aee864c092c4e1393e8079995f51745df
gui <995789897@qq.com>
2022-08-01 20:37:54 +0800

    commit 5（master）

--- src/main/java/com/gui/GitTest.java
+++ src/main/java/com/gui/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
-        System.out.println("一个测试master");
+        System.out.println("是一个测试master");
     }
 }
